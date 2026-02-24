01061851842ff3750e0a8a7b00f25c6d5f0fed8f
fjramirez1 <fj.ramirezlopez@um.es>
2026-02-24 18:16:28 +0100

    Productos (Spring Boot) creado. Rename productos a productos-aadd.

---
+++ productos/src/test/java/segundum/productos/ProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package segundum.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
