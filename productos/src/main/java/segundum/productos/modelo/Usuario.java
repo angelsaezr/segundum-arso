deba38475585770a970d3349ec198bf475df6944
fjramirez1 <fj.ramirezlopez@um.es>
2026-04-10 12:03:28 +0200

    Tarea 7: integridad del id de Usuario entre microservicios

--- productos/src/main/java/segundum/productos/modelo/Usuario.java
+++ productos/src/main/java/segundum/productos/modelo/Usuario.java
@@ -1,19 +1,14 @@
 package segundum.productos.modelo;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
 
 	@Id
-	@GeneratedValue(generator = "uuid")
-	@GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
 	private String id;
 
 	private String email;
